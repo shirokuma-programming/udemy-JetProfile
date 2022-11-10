e6a80ba1a0421f0a99beb005a98a5204921b5b10
shirokuma-programming <shirokuma2294@gmail.com>
2022-11-10 23:53:35 +0900

    名前と職業テキストの追加

--- app/src/main/java/com/example/udemy_jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/udemy_jetprofile/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.udemy_jetprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.udemy_jetprofile.ui.theme.UdemyJetProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +28,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        // 名前
+                        Text(
+                            text = "白井 熊子",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        // 職業
+                        Text(
+                            text = "職業：会社員",
+                            color = Color.Gray,
+                            fontSize = 16.sp
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    UdemyJetProfileTheme {
-        Greeting("Android")
     }
 }
