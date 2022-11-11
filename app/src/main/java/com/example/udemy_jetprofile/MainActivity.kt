d5030d75bacf9d397c1704cd552bb27c77f0fa14
shirokuma-programming <shirokuma2294@gmail.com>
2022-11-11 22:24:32 +0900

    プロフィール画像の追加、表示

--- app/src/main/java/com/example/udemy_jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/udemy_jetprofile/MainActivity.kt
@@ -3,16 +3,17 @@ package com.example.udemy_jetprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,8 +30,19 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(20.dp)
                     ) {
+                        // プロフィール画像
+                        Image(
+                            painter = painterResource(id = R.drawable.img_profile),
+                            contentDescription = "プロフィール画像",
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        
                         // 名前
                         Text(
                             text = "白井 熊子",
