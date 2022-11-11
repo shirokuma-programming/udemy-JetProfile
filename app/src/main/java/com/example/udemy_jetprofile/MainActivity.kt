55ddad979adf4db313284a48a50470c09ac59ee4
shirokuma-programming <shirokuma2294@gmail.com>
2022-11-11 23:39:11 +0900

    ラベルコンポーザブル関数の作成、趣味・居住地の追加

--- app/src/main/java/com/example/udemy_jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/udemy_jetprofile/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -81,20 +86,10 @@ class MainActivity : ComponentActivity() {
                             Spacer(modifier = Modifier.height(20.dp))
 
                             // メールアドレス
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.Email,
-                                    contentDescription = "メールアドレス"
-                                )
-                                Spacer(modifier = Modifier.width(10.dp))
-                                Text(
-                                    text = "Email",
-                                    fontSize = 14.sp,
-                                    fontWeight = FontWeight.Bold
-                                )
-                            }
+                            Label(
+                                icon = Icons.Default.Email,
+                                text = "Email"
+                            )
                             Spacer(modifier = Modifier.height(10.dp))
                             Text(
                                 text = "shiroikumako@email.com",
@@ -120,9 +115,54 @@ class MainActivity : ComponentActivity() {
                                 color = Color.White,
                             )
                         }
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        // 趣味・居住地
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Color.LightGray.copy(alpha = 0.3f))
+                                .padding(horizontal = 10.dp, vertical = 20.dp)
+                        ) {
+                            Label(
+                                icon = Icons.Default.Favorite,
+                                text = "趣味：散歩",
+                                color = Color.Gray
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Label(
+                                icon = Icons.Default.LocationOn,
+                                text = "居住地：北海道札幌市",
+                                color = Color.Gray
+                            )
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Label(
+    icon: ImageVector,
+    text: String,
+    color: Color = MaterialTheme.colors.onBackground
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = text,
+            color = color,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
